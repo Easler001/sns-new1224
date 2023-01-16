02c4bf1c353fa0450a6d00eb61a63a0ab1ff42d7
Easler <reflecter15@gmail.com>
2023-01-16 22:48:56 +0900

    change language

--- top/public/login_form.php
+++ top/public/login_form.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- top/public/signup_form.php
+++ top/public/signup_form.php
@@ -92,7 +92,7 @@ if (empty($error)) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
