78733b0c22c13969d53513675508d2a069fb8d91
Easler <reflecter15@gmail.com>
2023-01-21 23:49:19 +0900

    add file

---
+++ top/head/env.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', "us-cdbr-east-06.cleardb.net");
+define('DB_NAME', "heroku_3bf94c806b7c575");
+define('DB_USER', 'b2b4481a7a8f9d');
+define('DB_PASS', '4c3227dc');
+
+?>
--- top/head/library.php
+++ top/head/library.php
@@ -1,25 +1,37 @@
 <?php
 
+require_once('env.php');
+
+
 //htmlspecialcharsを短くする！(ファンクション化)
 function h($value) {
     return htmlspecialchars($value, ENT_QUOTES);
 }
 /* DBへの接続 */
-function dbconnect() {
-  $db = new mysqli("us-cdbr-east-06.cleardb.net", 'b2b4481a7a8f9d', '4c3227dc', "heroku_3bf94c806b7c575");
-  if (!$db) {
-		die($db->error);
-	}
-
+function dbConnect() {
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $host   = DB_HOST;
+  $dbname = DB_NAME;
+  $user   = DB_USER;
+  $pass   = DB_PASS;
+  $dsn    = "mysql:host=$host;dbname=$dbname;charset=utf8";
+  $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
   $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
   );
 
-
-  return $db;
-}
+  
+  try {
+      $dbh = new PDO($dsn,$user,$pass,[
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ]);
+  } catch(PDOException $e){
+      echo '接続失敗' . $e->getMessage();
+      exit();
+  };
+  return $dbh;
 function setCategoryName($caregory) {
   if ($caregory === '1') {
     return '日常';
@@ -47,4 +59,12 @@ function setCategoryName($caregory) {
 
 //localの時
 //$db = new mysqli('localhost', 'root', 'root', 'sns-new');
+
+//function dbconnect() {
+//  $db = new mysqli("us-cdbr-east-06.cleardb.net", 'b2b4481a7a8f9d', '4c3227dc', "heroku_3bf94c806b7c575");
+//  if (!$db) {
+//		die($db->error);
+	}
+
+
 ?>
