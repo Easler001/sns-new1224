0aea32a69895387d3db70d48dd66e768a054aab5
Easler <reflecter15@gmail.com>
2023-05-30 22:23:32 +0900

    5/30

--- top/head/env.php
+++ top/head/env.php
@@ -6,9 +6,15 @@
 //define('DB_PASS', 'root');
 
 
-define('DB_HOST', "us-cdbr-east-06.cleardb.net");
-define('DB_NAME', "heroku_9fc78d20c59e2b2");
-define('DB_USER', 'b98fe82ffaef7f');
-define('DB_PASS', '4b5f50f5');
+//define('DB_HOST', "us-cdbr-east-06.cleardb.net");
+//define('DB_NAME', "heroku_9fc78d20c59e2b2");
+//define('DB_USER', 'b98fe82ffaef7f');
+//define('DB_PASS', '4b5f50f5');
+
+
+define('DB_HOST', "mysql212.phy.lolipop.lan");
+define('DB_NAME', "LAA1526646-incolle");
+define('DB_USER', 'LAA1526646');
+define('DB_PASS', 'incolle1224');
 
 ?>
